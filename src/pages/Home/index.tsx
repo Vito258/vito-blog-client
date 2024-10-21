20ff75d1759e225d2d0cad5755ce2db819a10ef8
Vito258 <3090095224@qq.com>
2024-10-21 15:38:53 +0800

    首页新增 一个简易的跑马灯组件

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,39 @@
+import Carousel from "react-material-ui-carousel";
+import { Paper, Button } from "@mui/material";
+import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
+import "./style.scss";
+
+function Item(props) {
+  return (
+    <Paper>
+      <h2>{props.item.name}</h2>
+      <p>{props.item.description}</p>
+
+      <Button className="CheckButton">Check it out!</Button>
+    </Paper>
+  );
+}
 
 function Home() {
-  
+  var items = [
+    {
+      name: "Random Name #1",
+      description: "Probably the most random thing you have ever seen!",
+    },
+    {
+      name: "Random Name #2",
+      description: "Hello World!",
+    },
+  ];
+
   return (
-    <div>
-      
+    <div className="home-container">
+      <Carousel className="home-carousel" PrevIcon={<ArrowBackIosNewOutlinedIcon/>} NextIcon={<ArrowForwardIosOutlinedIcon/>}>
+        {items.map((item, i) => (
+          <Item key={i} item={item} />
+        ))}
+      </Carousel>
     </div>
   );
 }
