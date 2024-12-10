a53bff35c9f5b2c3efbd1318d5f51564250158ef
Vito258 <3090095224@qq.com>
2024-12-10 10:30:42 +0800

    将draft.js 更改为wangEditor

--- src/components/RichTextEditor/index.tsx
+++ src/components/RichTextEditor/index.tsx
@@ -1,48 +1,63 @@
-import { useState } from "react";
-import { EditorState } from "draft-js";
-import "draft-js/dist/Draft.css";
-import Box from "@mui/material/Box";
-import Editor from '@draft-js-plugins/editor';
-import createHashtagPlugin from '@draft-js-plugins/undo';
-import createLinkifyPlugin from '@draft-js-plugins/undo';
-import "./style.scss";
+import { useState, useEffect } from "react";
 
-interface RichTextEditorProps {
-  // 编辑状态
-  editorState?: EditorState;
-  // 编辑状态改变的回调
-  onEditorChange?: (newEditorState: EditorState) => void;
-}
+import Box from "@mui/material/Box";
+
+import "@wangeditor/editor/dist/css/style.css"; // 引入 css
+
+import { Editor, Toolbar } from "@wangeditor/editor-for-react";
+import { IDomEditor, IEditorConfig, IToolbarConfig } from "@wangeditor/editor";
+import "./style.scss";
 
 /**
  * 富文本编辑器组件
- * @param editorState
- * @param onEditorChange
  * @returns
  */
-function RichTextEditor({ editorState, onEditorChange }: RichTextEditorProps) {
-  const [state, setState] = useState(editorState || EditorState.createEmpty());
+function RichTextEditor() {
+  // editor 实例
+  const [editor, setEditor] = useState<IDomEditor | null>(null);
 
-  const handleEditorChange = (newEditorState: EditorState) => {
-    setState(newEditorState);
-    if (onEditorChange) {
-      onEditorChange(newEditorState);
-    }
+  // 编辑器内容
+  const [html, setHtml] = useState("<p>hello</p>");
+
+  // 模拟 ajax 请求，异步设置 html
+  useEffect(() => {
+    setTimeout(() => {
+      setHtml("<p>hello world</p>");
+    }, 1500);
+  }, []);
+
+  // 工具栏配置
+  const toolbarConfig: Partial<IToolbarConfig> = {};
+
+  // 编辑器配置
+  const editorConfig: Partial<IEditorConfig> = {
+    placeholder: "请输入内容...",
   };
 
-  //  创建编辑器所用到的插件
-  const hashtagPlugin = createHashtagPlugin();
-  const linkifyPlugin = createLinkifyPlugin();
-
-  const plugins = [linkifyPlugin, hashtagPlugin];
+  // 及时销毁 editor ，重要！
+  useEffect(() => {
+    return () => {
+      if (editor == null) return;
+      editor.destroy();
+      setEditor(null);
+    };
+  }, [editor]);
 
   return (
     <Box className="RichTextEditor">
+      <Toolbar
+        editor={editor}
+        defaultConfig={toolbarConfig}
+        mode="default"
+        style={{ borderBottom: "1px solid #ccc" }}
+      />
       <Editor
-        editorState={state}
-        onChange={handleEditorChange}
-        plugins={plugins}
-        placeholder="Write your article here..."
+        defaultConfig={editorConfig}
+        value={html}
+        onCreated={setEditor}
+        onChange={(editor) => setHtml(editor.getHtml())}
+        mode="default"
+        style={{ height: "500px", overflowY: "hidden" }}
       />
     </Box>
   );
